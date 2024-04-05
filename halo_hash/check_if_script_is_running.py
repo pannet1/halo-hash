f494be56c1712af9f99b2ae92e5a320f156c7609
Maheswaran Palaniselvan <maheswaran.palaniselvan@gmail.com>
2024-04-05 12:22:44 +0530

    fix

--- halo_hash/check_if_script_is_running.py
+++ halo_hash/check_if_script_is_running.py
@@ -4,6 +4,7 @@ from constants import TGRAM
 for process in psutil.process_iter():
     if process.cmdline() == ['python', 'main_telegram.py']:
         TGRAM.send_msg("Good Morning, The script has started running!")
-    else:
-        TGRAM.send_msg("Good Morning, The script has not started today, please check!")
+        break
+else:
+    TGRAM.send_msg("Good Morning, The script has not started today, please check!")
 
